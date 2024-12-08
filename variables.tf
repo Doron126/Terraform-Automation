1d44d9b601fbc28e6821c0dd133332a7a00edd06
Doron126 <doronshamoco@gmail.com>
2024-12-08 07:48:43 +0000

    Adding files

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vsphere_user" {
+  description = "vsphere admin username"
+  type        = string
+}
+
+variable "vsphere_password" {
+  description = "vsphere admin password"
+  type        = string
+}
+
+variable "vsphere_server" {
+  description = "vsphere server address"
+  type        = string
+}
+
+variable "machine_password" {
+  description = "machine user password"
+  type        = string
+}
